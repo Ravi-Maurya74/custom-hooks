0d64ae1973339728ca0ce45c1913bf3baa085d3d
Ravi <lcs2020043@iiitl.ac.in>
2024-01-16 16:33:49 +0530

    complete

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,19 +3,21 @@ import { useState, useEffect } from "react";
 export function useFetch(fetchFn, initialValue) {
   const [isFetching, setIsFetching] = useState(false);
   const [error, setError] = useState();
-  const [fetchedData, setFetchedData] = useState(initialValue);
+  const [fetchedData, setFetchedData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       setIsFetching(true);
       try {
         const data = await fetchFn();
+        console.log(data);
         setFetchedData(data);
       } catch (err) {
         setError({ message: err.message || "Something went wrong" });
       }
       setIsFetching(false);
     }
+    fetchData();
   }, [fetchFn]);
-  return { isFetching, fetchedData, error };
+  return { isFetching, fetchedData, error, setFetchedData };
 }
